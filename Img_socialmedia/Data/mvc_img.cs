2609e17d239c36acd4039b2d535a774688828dbf
Đạt Nguyễn <Q.Dat@outlook.com>
2020-05-25 15:00:44 +0700

    layout page

---
+++ Img_socialmedia/Data/mvc_img.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Img_socialmedia.Models;
+
+namespace Img_socialmedia.Data
+{
+    public class mvc_img:DbContext
+    {
+        public mvc_img(DbContextOptions<mvc_img> options):base(options)
+        {
+
+        }
+
+        public DbSet<UserViewModel> users { get; set; }
+    }
+}
---
+++ Img_socialmedia/Models/UserViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Img_socialmedia.Models
+{
+    public class UserViewModel
+    {
+        public string id { get; set; }
+        public string username { get; set; }
+        public string name { get; set; }
+        public string frist_name { get; set; }
+        public string last_name { get; set; }
+        public string bio { get; set; }
+        public string location { get; set; }
+        public string instagram_username { get; set; }
+        public string twitter_username { get; set; }
+        public int total_like { get; set; }
+        public int total_photos { get; set; }
+        public int total_collections { get; set; }
+        public int follower_count { get; set; }
+        public int following_count { get; set; }
+        public int downloads { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime update_at { get; set; }
+    }
+}

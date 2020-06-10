dad4e13343e1cc5fd69df53285841af9fadfa790
Đạt Nguyễn <Q.Dat@outlook.com>
2020-06-10 23:43:33 +0700

    signup

---
+++ Img_socialmedia/Controllers/SignUpController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Img_socialmedia.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.showtopbar = false;
+
+            return View();
+        } 
+
+
+        public IActionResult signup()
+        {
+            return View();
+        }
+    }
+}

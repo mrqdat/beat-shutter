4a1d3d110a259019dfc15c8240314b704db92535
Đạt Nguyễn <Q.Dat@outlook.com>
2020-06-21 06:52:42 +0700

    user page

---
+++ Img_socialmedia/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Img_socialmedia.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult editprofile()
+        {
+            return View();
+        }
+    }
+}
